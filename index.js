83c702e4c6bf9725682f4169e9263cd6fb6165f1
matt roche <matt.roche@gmail.com>
2023-02-22 21:19:14 -0600

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+{
+let num1 = 2;
+let num2 = 31;
+
+let multiply = (num1) * (num2)};
+
+{
+    let num3 = 24;
+    let num4 = 6;
+
+    let mod = (num3) / (num4)};
+
+
+{let random = Math.ceil(Math.random() * 10)};
+
+{let max = Math.max(1, 4, 8, 16, 20);};
