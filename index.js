6dcfcac9c0a7271eee82c8924b5a257674416777
matt roche <matt.roche@gmail.com>
2023-03-19 01:23:41 -0500

    completed phase 1 arithmetic lab

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
-{
+
 let num1 = 2;
 let num2 = 31;
+const multiply = (num1) * (num2);
 
-let multiply = (num1) * (num2)};
+const random = (Math.ceil((Math.random()) * (10)));
 
-{
-    let num3 = 24;
-    let num4 = 6;
+let num3 = 14;
+let num4 = 5;
+const mod = (num3) % (num4);
 
-    let mod = (num3) / (num4)};
-
-
-{let random = Math.ceil(Math.random() * 10)};
-
-{let max = Math.max(1, 4, 8, 16, 20);};
+const max = (Math.max(1, 2, 3, 20));
